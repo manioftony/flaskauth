078fa1bfacbd2e859663d23841fb3f308544b82a
manikandan <kadhirmani@gmail.com>
2016-12-25 12:13:14 +0530

    new changes done

--- app.py
+++ app.py
@@ -1,20 +1,88 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mani/gitproject/flaskauth/artelus.db'
+app.config['SECRET_KEY']='super-secret'
+# app.config['SECURITY_REGISTERABLE'] = True
+db = SQLAlchemy(app)
 
 
 
 
 
 
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+# # Create a user to test with
+# @app.before_first_request
+# def create_user():
+#     db.create_all()
+#     user_datastore.create_user(email='mani@gmail.com', password='mani')
+#     db.session.commit()
+
+
+
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     username = db.Column(db.String(120),unique=True)
+#     email = db.Column(db.String(120),unique=True)
+#     # password = db.Column(db.String(120),primary_key=True)
+
+#     def __init__(self,username,email):
+#         self.username = username
+#         self.email = email
+#     def __repr__(self):
+#         return "<User %r>"%self.username
+
+
+
+
+
+@login_required
 @app.route('/')
 def index():
-    return "Hello World"
+    obj = User.query.all()
+    return render_template('table.html',**locals())
 
+@app.route('/home')
+def home():
 
+    data = "manikandan"
+    return render_template('home.html',**locals())
 
-
-
+@app.route('/post_user',methods=['POST'])
+def post_user():
+    user = User(request.form['username'],request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 
---
+++ apps.py
@@ -0,0 +1,122 @@
+from flask import Flask,render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from werkzeug.security import generate_password_hash, \
+     check_password_hash
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mani/gitproject/flaskauth/karthik.db'
+db = SQLAlchemy(app)
+
+
+
+@app.route('/')
+def index():
+
+    return "fddghfdh"
+
+
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     username = db.Column(db.String(120),unique=True)
+#     password = db.Column(db.String(120),unique=True)
+
+
+#     # def __init__(self,username,email):
+#     #     self.username = username
+#     #     self.email = email
+#     # def __repr__(self):
+#     #     return "<User %r>"%self.username
+
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.set_password(password)
+
+
+#     def set_password(self, password):
+#         self.pw_hash = generate_password_hash(password)
+
+#     def check_password(self, password):
+#         return check_password_hash(self.pw_hash, password)
+from passlib.apps import custom_app_context as pwd_context
+
+
+class User(db.Model,UserMixin):
+    # __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(128))
+
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+
+
+
+
+
+
+
+from flask_login import login_user,LoginManager
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+
+
+
+
+
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+    import ipdb;ipdb.set_trace()
+    if request.method == 'POST':
+         # username = request.json.get('username')
+         # password = request.json.get('password')
+         username = request.form['username']
+         password = request.form['password']
+         user = User.query.filter_by(username = username).first()
+         if not user or not user.verify_password(password):
+            return False
+         login_user(user)
+         # return redirect(url_for('index'))
+         return "successfully registered"
+    if request.method == 'GET':
+        return render_template('register.html',**locals())
+
+
+
+
+
+
+def usercreation():
+    db.create_all()
+    obj = User(username="mani")
+    obj.hash_password("mani")
+    db.session.add(obj)
+    db.session.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
